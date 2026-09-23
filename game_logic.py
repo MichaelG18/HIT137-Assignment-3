06ccbcf558dc009605fb654b936413126f359bd1
MichaelG18 <michaelginis246@gmail.com>
2026-09-23 12:45:10 +0930

    Add PuzzleGame class for image puzzle logic

--- game_logic.py
+++ game_logic.py
@@ -47,3 +47,69 @@ class Tile:
             and self._rotation == 0
             and not self._flipped
         )
+
+class PuzzleGame:
+    """Controls the main state and logic of the image puzzle game."""
+
+    VALID_GRID_SIZES = (3, 4, 5)
+
+    def __init__(self, grid_size=3):
+        self._grid_size = None
+        self._tiles = []
+        self._moves = 0
+        self._selected_position = None
+        self._hints_used = 0
+        self._game_complete = False
+
+        self.new_game(grid_size)
+
+    @property
+    def grid_size(self):
+        return self._grid_size
+
+    @property
+    def moves(self):
+        return self._moves
+
+    @property
+    def tiles(self):
+        return tuple(self._tiles)
+
+    @property
+    def selected_position(self):
+        return self._selected_position
+
+    @property
+    def hints_used(self):
+        return self._hints_used
+
+    @property
+    def game_complete(self):
+        return self._game_complete
+
+    def new_game(self, grid_size):
+        """Create a fresh puzzle using the selected grid size."""
+
+        if grid_size not in self.VALID_GRID_SIZES:
+            raise ValueError("Grid size must be 3, 4 or 5.")
+
+        self._grid_size = grid_size
+        self._moves = 0
+        self._selected_position = None
+        self._hints_used = 0
+        self._game_complete = False
+
+        tile_count = grid_size * grid_size
+
+        self._tiles = [
+            Tile(tile_id=index, correct_position=index)
+            for index in range(tile_count)
+        ]
+
+    def tiles_left(self):
+        """Return the number of tiles that are not currently correct."""
+        return sum(not tile.is_correct() for tile in self._tiles)
+
+    def is_complete(self):
+        """Return True when every tile is correctly restored."""
+        return self.tiles_left() == 0
